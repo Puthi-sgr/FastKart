bf1ab90f7e7241b0fe1588060c607b542068ef70
Puthi-sgr <chethaputhi@gmail.com>
2025-08-20 10:50:28 +0700

    Solved the search mobile view

---
+++ dist/fastkart-frontend-rest/browser/chunk-CMHHQ6NN.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-KL66OKBH.js";import"./chunk-WLEDIL6Y.js";import"./chunk-BA6ICZWT.js";import"./chunk-WCEYXBMB.js";import"./chunk-VKKTQPIP.js";export{a as BlogModule};
---
+++ dist/fastkart-frontend-rest/browser/chunk-IWKSH32Z.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-WKBUVR4W.js";import"./chunk-RZOH3XTD.js";import"./chunk-BA6ICZWT.js";import"./chunk-CDTSNH7I.js";import"./chunk-WCEYXBMB.js";import"./chunk-VKKTQPIP.js";export{a as ShopModule};
